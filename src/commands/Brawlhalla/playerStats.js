2374240bf47ebc74bb795553a343d07eb624486a
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2023-03-29 22:30:41 -0400

    updated playerStats

--- src/commands/Brawlhalla/playerStats.js
+++ src/commands/Brawlhalla/playerStats.js
@@ -13,16 +13,23 @@ callback: async(client, interaction) => {
 
     const playerName = interaction.options.get('player-name').value;  
     var options = {
-        perfect_match: false,
-        unique: false
+        perfect_match: true,
+        unique: true
     }   
 
     try{
        
-        playerStats = bh_api.findPlayer(playerName, options).catch(err => console.log(err)).then(playerStats => {console.log(playerStats);
+        playerStats = bh_api.findPlayer(playerName).catch(err => console.log(err)).then(playerStats => {
+            for (let [key, value, brawhallaStats] of Object.entries(playerStats)) {
+                if (value.name == playerName){
+                    brawhallaStats = bh_api.fetchPlayerStats(value.brawlhalla_id).then(brawhallaStats =>{
+                        console.log(brawhallaStats);
+                    })
+                    
+                }
+            }
+            //await interaction.reply(`${playerStats}`);
         })
-        //await interaction.reply(`${playerStats}`);
-
         return;
     }catch(error){
 
